8aa9891acdfa309e9b2ea23151955a6dc8478392
Jan Friedli <jan.friedli@immerda.ch>
2021-07-09 08:43:34 +0000

    linted

--- deploy/firefox.js
+++ deploy/firefox.js
@@ -1,27 +1,32 @@
-const editJsonFile = require("edit-json-file");
-let file = editJsonFile(`${__dirname}/../src/manifest.json`);
+const editJsonFile = require('edit-json-file');
+
+const file = editJsonFile(`${__dirname}/../src/manifest.json`);
 
 // Make the necessary changes to the manifest
 // To ensure firefox compatibility
-file.set("manifest_version", 2);
+file.set('manifest_version', 2);
 file.append(
-    "permissions", "https://bzcookie.fans/*"
-)
+  'permissions', 'https://bzcookie.fans/*',
+);
+/* eslint-disable no-console */
+
 console.log(file.get());
 file.save();
 
-const zipFolder = require('./zipFolder.js')
-const browser = 'firefox'
+const zipFolder = require('./zipFolder');
 
-let folder = `src`
-let zipName = `${browser}.zip`
+const browser = 'firefox';
 
+const folder = 'src';
+const zipName = `${browser}.zip`;
 
 zipFolder(folder, zipName)
-	.then(() => {
-		console.log(`Successfully Zipped ${folder} and saved as ${zipName}`)
-	})
-	.catch(err => {
-		console.log('Can not create zip:', err)
-		process.exit(1)
-	})
+  .then(() => {
+    /* eslint-disable no-console */
+    console.log(`Successfully Zipped ${folder} and saved as ${zipName}`);
+  })
+  .catch((err) => {
+    /* eslint-disable no-console */
+    console.log('Can not create zip:', err);
+    process.exit(1);
+  });
