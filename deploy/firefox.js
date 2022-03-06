acdcabb76d1062fb3b30d521df56b423046cc035
jf <jan.friedli@immerda.ch>
2022-03-06 18:34:42 +0100

    refactor require in ff deploy script

--- deploy/firefox.js
+++ deploy/firefox.js
@@ -1,3 +1,6 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 const editJsonFile = require('edit-json-file');
 
 const file = editJsonFile(`${__dirname}/../src/manifest.json`);
